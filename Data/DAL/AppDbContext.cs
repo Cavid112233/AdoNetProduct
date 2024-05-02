40aa2fcdc695b6571527164e06de0d1768fb143f
Cavid112233 <suleymanli535@gmail.com>
2024-05-03 01:49:07 +0400

    Add project files.

---
+++ Data/DAL/AppDbContext.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Data.DAL
+{
+    public class AppDbContext
+    {
+        string conString = "Server=DESKTOP-DMS4347\\SQLEXPRESS01;Database=AdoNetAB204;Trusted_Connection=true;Integrated Security=true;TrustServerCertificate=true;";
+        public SqlConnection sqlConnection;
+
+        public AppDbContext()
+        {
+            sqlConnection = new SqlConnection(conString);
+        }
+
+        public void NonQuery(string command)
+        {
+            sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+            var result = sqlCommand.ExecuteNonQuery();
+
+            if (result > 0)
+            {
+                Console.WriteLine("Completed successfully: " + result);
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong!");
+            }
+            sqlConnection.Close();
+        }
+
+        public DataTable Query(string selectCommand)
+        {
+            sqlConnection.Open();
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(selectCommand, sqlConnection);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            sqlConnection.Close();
+            return dataTable;
+        }
+    }
+}
---
+++ Data/RepositoryConcretes/ProductRepository.cs
@@ -0,0 +1,100 @@
+﻿using Core.Models;
+using Core.RepositoryAbstract;
+using Data.DAL;
+using System.Data;
+
+namespace Data.RepositoryConcretes
+{
+    public class ProductRepository : IProductRepository
+    {
+        AppDbContext _appDbContext = new AppDbContext();
+        public void Add(string command)
+        {
+            _appDbContext.NonQuery(command);
+        }
+
+        public void Delete(string command)
+        {
+            _appDbContext.NonQuery(command);
+        }
+
+        public Product Get(string command) 
+        {
+            var dt = _appDbContext.Query(command);
+            Product product = null;
+            foreach (DataRow item in dt.Rows)
+            {
+                product = new Product
+                {
+                    Id = int.Parse(item[0].ToString()),
+                    Name = item[1].ToString(),
+                    Price = double.Parse(item[2].ToString())
+                };
+            }
+
+            return product;
+        }
+
+        public List<Product> GetAll(string command)
+        {
+            var dt = _appDbContext.Query(command);
+            List<Product> products = new List<Product>();
+
+            foreach (DataRow item in dt.Rows)
+            {
+                Product product = new Product
+                {
+                    Id = int.Parse(item[0].ToString()),
+                    Name = item[1].ToString(),
+                    Price = double.Parse(item[2].ToString())
+                };
+
+                products.Add(product);
+            }
+
+            return products;
+        }
+
+
+        Product IProductRepository.Get(string command)
+        {
+            var dt = _appDbContext.Query(command);
+            Product product = null;
+            foreach (DataRow item in dt.Rows)
+            {
+                product = new Product
+                {
+                    Id = int.Parse(item[0].ToString()),
+                    Name = item[1].ToString(),
+                    Price = double.Parse(item[2].ToString())
+                };
+            }
+
+            return product;
+        }
+
+        List<Product> IProductRepository.GetAll(string command)
+        {
+            var dt = _appDbContext.Query(command);
+            List<Product> products = new List<Product>();
+
+            foreach (DataRow item in dt.Rows)
+            {
+                Product student = new Product
+                {
+                    Id = int.Parse(item[0].ToString()),
+                    Name = item[1].ToString(),
+                    Price = double.Parse(item[2].ToString())
+                };
+
+                products.Add(student);
+            }
+
+            return products;
+        }
+        public void Update(string command)
+        {
+            _appDbContext.NonQuery(command);
+        }
+    }
+}
